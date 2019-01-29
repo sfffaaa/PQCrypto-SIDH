c3b24915e058d951887179101ee2a7df560940e1
jaypan <pan@advancedblockchain.com>
2019-01-29 15:01:18 +0100

    Consistency with other encryption comparisons

--- tests/mytest_SIKEp751.c
+++ tests/mytest_SIKEp751.c
@@ -34,10 +34,10 @@
 // Benchmark and test parameters
 #if defined(OPTIMIZED_GENERIC_IMPLEMENTATION) || (TARGET == TARGET_ARM)
     #define BENCH_LOOPS        5      // Number of iterations per bench
-    #define TEST_LOOPS         10      // Number of iterations per test
+    #define TEST_LOOPS         50      // Number of iterations per test
 #else
     #define BENCH_LOOPS       100
-    #define TEST_LOOPS        10
+    #define TEST_LOOPS        50
 #endif
 
 #define TEST_JSON_PLAINTEXT "{\n" \
